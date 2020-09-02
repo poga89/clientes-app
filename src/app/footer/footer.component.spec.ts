7c1ef0cf42bbe9f44ca873b9a3aae478081ab5ae
Polo <poga89@hotmail.com>
2020-09-01 22:04:19 -0500

    despues del error en la actualizacion

--- src/app/footer/footer.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FooterComponent } from './footer.component';
-
-describe('Footer.Component', () => {
-  it('should create an instance', () => {
-    expect(new FooterComponent()).toBeTruthy();
-  });
-});
